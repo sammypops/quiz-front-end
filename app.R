960041317a9afa4e34a2a9eb497be9bf451d1385
Sam Hunter <samhunter000@gmail.com>
2023-02-06 12:30:04 +0000

    init commit

---
+++ app.R
@@ -0,0 +1,26 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+## app.R ##
+library(shinydashboard)
+
+source("ui.R", local = T)
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
